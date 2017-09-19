1f76e8f7cc06544550a00ff63375140201111b81
ole <olemoudi@gmail.com>
2017-09-19 18:50:21 +0200

    added git check

--- configure.sh
+++ configure.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
+command -v git >/dev/null 2>&1 || { echo "I require git but it's not installed.  Aborting." >&2; exit 1; }
 mkdir -p ~/.vim
 cp -r * ~/.vim/
 cat olevimrc.vim >> ~/.vimrc
 mkdir -p ~/.fonts
 cp fonts/powerline/DejaVuSansMono/* ~/.fonts/
 fc-cache -vf ~/.fonts/
+cd ~/.vim
 git submodule foreach git pull origin master
 
